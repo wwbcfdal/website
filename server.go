7305032f30f1fc591f1707950e09f1122c952948
alaneric1618 <alaneric1618@gmail.com>
2016-10-20 21:48:11 -0500

    Website file structure

--- server.go
+++ server.go
@@ -3,15 +3,41 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 )
 
+func MarkdownRenderHandler(w http.ResponseWriter, r *http.Request) {
+	s := "dynamic data"
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(s))
+}
 
 func main() {
-	fs := http.FileServer(http.Dir("static"))
-	http.Handle("/", fs)
+	// Primary servermux routes
+	routes := http.NewServeMux()
 
-	log.Println("Listening...")
-	err := http.ListenAndServe(":80", nil)
+	// Static server
+	fs := http.FileServer(http.Dir("static"))
+	// Init routes
+	routes.Handle("/", fs)
+	routes.Handle("/js/", fs)
+	routes.Handle("/css/", fs)
+	routes.Handle("/fonts/", fs)
+	routes.Handle("/images/", fs)
+	routes.HandleFunc("/markdown/", MarkdownRenderHandler)
+	
+	log.Println("Listening on port: 80")
+
+	// Create Server
+	server := http.Server{
+		Addr: ":http",
+		Handler: routes,
+		ReadTimeout: time.Second*10,
+		WriteTimeout: time.Second*10,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	err := server.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
